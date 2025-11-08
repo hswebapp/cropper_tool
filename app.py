df3a1c4ca1699bfbc9fcdea9a2a5fbda4d73b8de
LA@9pNFVPRAL7jq <hswebapp2025@gmail.com>
2025-11-09 01:17:30 +0500

    changed cropped to new font

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def crop_aadhaar():
 
     cropped_imgs = []
     for idx, page in enumerate(pages, 1):
-        cropped = crop_bottom_half(page, crop_ratio=0.298)  # adjust ratio if needed
+        cropped = crop_bottom_half(page, crop_ratio=0.283)  # adjust ratio if needed
         cropped_imgs.append((f"aadhaar_page_{idx}.png", cropped))
 
     if len(cropped_imgs) == 1:
